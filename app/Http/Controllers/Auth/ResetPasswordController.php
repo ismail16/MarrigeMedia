2e75e6d1b2953c66cde31717a40c6183f4efa9ad
ismail16 <ismail32cse@gmail.com>
2021-08-11 06:14:25 +0600

    reset ppassword

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -8,23 +8,22 @@ use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class ResetPasswordController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset requests
-    | and uses a simple trait to include this behavior. You're free to
-    | explore this trait and override any methods you wish to tweak.
-    |
-    */
+
 
     use ResetsPasswords;
 
-    /**
-     * Where to redirect users after resetting their password.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo;
+
+    public function __construct()
+    {
+        $this->middleware('guest');
+
+        if (Auth::check() && Auth::user()->role->id == 1) {
+            $this->redirectTo = route('admin.dashboard');
+        } else {
+            $this->redirectTo = route('member.dashboard');
+        }
+        $this->middleware('guest')->except('logout');
+
+    }
 }
