0b60614cc1a08f9aec4e2f58cea13ed34e4f9652
ismail1199 <ismail32cse@gmail.com>
2021-09-26 18:01:32 +0600

    regi validation

--- app/Http/Controllers/Admin/UserInfoController.php
+++ app/Http/Controllers/Admin/UserInfoController.php
@@ -72,6 +72,21 @@ class UserInfoController extends Controller
 
     public function destroy($id)
     {
-        //
+        $user = User::find($id);
+        $PersonalInfo = PersonalInfo::where('user_id', $user->id)->first();
+        if ($PersonalInfo) {
+             $PersonalInfo->delete();
+        }
+        $UserProfileImages = UserProfileImage::where('user_id', $user->id)->get();
+        if ($UserProfileImages) {
+             $UserProfileImages->delete();
+        }
+        // $receive_proposal = Proposal::where('receive_proposal_user', Auth::user()->id)->get();
+        // $sent_messages =  PrivateMessages::where('from_id', $user->id)->orderBy('id', 'desc')->get();
+        $preference =  Preference::where('user_id', $user->id)->first();
+
+        $success_story->delete();
+        return redirect()->route('admin.success-story.index')->with('message','Success Story Delete Successfully.');
+
     }
 }
