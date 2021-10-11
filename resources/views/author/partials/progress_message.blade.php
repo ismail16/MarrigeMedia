d5a81e55381c14d47804c37ef801709022955317
ismail1199 <ismail32cse@gmail.com>
2021-10-11 18:03:37 +0600

    progress message

--- resources/views/author/partials/progress_message.blade.php
+++ resources/views/author/partials/progress_message.blade.php
@@ -1,25 +1,26 @@
+<?php 
+    $user = Auth::user();
+    $PersonalInfo = \App\Models\PersonalInfo::where('user_id', $user->id)->first();
+    $UserProfileImage = \App\Models\UserProfileImage::where('user_id', $user->id)->first();
+    $Preference = \App\Models\Preference::where('user_id', $user->id)->first();
+
+    $a = 20;
+    if ($PersonalInfo) {
+        $b = 40;
+    }else{ $b=0; }
+    if ($UserProfileImage) {
+        $c = 20;
+    }else{ $c=0; }
+    if ($Preference) {
+        $d = 20;
+    }else{ $d=0; }
+    $persent = $a+$b+$c+$d;
+?>
+@if(!$PersonalInfo || !$PersonalInfo || !$PersonalInfo)
 <div class="alert alert-warning alert-dismissible pt-3 pr-4 pl-4 pb-3 fade show mt-2" role="alert">
     <button type="button" class="close text-danger pt-0 pr-1" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
       </button>
-        <?php 
-        $user = Auth::user();
-        $PersonalInfo = \App\Models\PersonalInfo::where('user_id', $user->id)->first();
-        $UserProfileImage = \App\Models\UserProfileImage::where('user_id', $user->id)->first();
-        $Preference = \App\Models\Preference::where('user_id', $user->id)->first();
-
-        $a = 20;
-        if ($PersonalInfo) {
-            $b = 40;
-        }else{ $b=0; }
-        if ($UserProfileImage) {
-            $c = 20;
-        }else{ $c=0; }
-        if ($Preference) {
-            $d = 20;
-        }else{ $d=0; }
-        $persent = $a+$b+$c+$d;
-    ?>
     <div class="progress mb-2 text-center">
       <div class="progress-bar bg-primary" role="progressbar" style="width: {{$persent}}%;" aria-valuenow="{{$persent}}" aria-valuemin="0" aria-valuemax="100">
           <strong>{{$persent}}%</strong>
@@ -40,3 +41,4 @@
     </span>
       
 </div>
+@endif
